e6cdba81cb261fa4a4d1bd1d52f6effce103ab0e
Leeuwestein <nathan.leeuwestein.dev@gmail.com>
2023-07-11 15:44:13 +0200

    5 sliders added total

--- src/app/location-slider/location-slider.component.ts
+++ src/app/location-slider/location-slider.component.ts
@@ -10,6 +10,8 @@ export class LocationSliderComponent {
   @ViewChild('slickMain', { static: false }) slickMain: any;
   @ViewChild('slickMainTitle', { static: false }) slickMainTitle: any;
   @ViewChild('slickNext', { static: false }) slickNext: any;
+  @ViewChild('slickNextTitle', { static: false }) slickNextTitle: any;
+  @ViewChild('slickLowerLocation', { static: false }) slickLowerLocation: any;
 
   constructor(
     public sliderService: SliderService,
@@ -19,6 +21,8 @@ export class LocationSliderComponent {
   mainImageSlider: any;
   mainTitleSlider: any;
   nextImageSlider: any;
+  nextImageTitle: any;
+  lowerLocation: any;
 
   ngAfterViewInit() {
     this.mainImageSlider =
@@ -27,39 +31,46 @@ export class LocationSliderComponent {
       this.elementRef.nativeElement.querySelector('.slider-main-title');
     this.nextImageSlider =
       this.elementRef.nativeElement.querySelector('.slider-next-image');
+    this.nextImageTitle =
+      this.elementRef.nativeElement.querySelector('.slider-next-title');
+    this.lowerLocation = this.elementRef.nativeElement.querySelector(
+      '.slider-lower-location'
+    );
   }
 
   ngOnInit() {
     this.sliderService.registerSlider(this);
-
-    console.log(this.calculateTotalExp(64));
   }
 
   slides = [
     {
       img: 'https://images.unsplash.com/photo-1688168293343-e1c824a4ace5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1100&h=670&q=80',
       title: 'Egypt',
+      location: 'The Giza pyramid complex - El Cairo Egypt',
     },
     {
       img: 'https://images.unsplash.com/photo-1596407628318-9541e92f40a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1100&h=670&q=80',
       title: 'Maldive',
+      location: 'Mahé - Seychelles islands',
     },
     {
       img: 'https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1100&h=670&q=80',
       title: 'Rome',
+      location: 'Collosseum - Rome, Italy',
     },
     {
       img: 'https://images.unsplash.com/photo-1504019347908-b45f9b0b8dd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1100&h=670&q=80',
       title: 'Greece',
+      location: 'Zakynthos Blue - Zakynthos, Greece',
     },
   ];
 
   nextSlide() {
-    if (this.slickMain && this.slickNext && this.mainTitleSlider) {
-      this.slickMain.slickNext();
-      this.slickNext.slickNext();
-      this.slickMainTitle.slickNext();
-    }
+    this.slickMain.slickNext();
+    this.slickNext.slickNext();
+    this.slickMainTitle.slickNext();
+    this.slickNextTitle.slickNext();
+    this.slickLowerLocation.slickNext();
   }
 
   nextSlides = [...this.slides.slice(1), this.slides[0]];
@@ -83,12 +94,13 @@ export class LocationSliderComponent {
     speed: 1000,
   };
 
-  calculateTotalExp(level: number) {
-    let totalExp = 0;
-    for (let i = 1; i <= level; i++) {
-      let expRequired = i * 1000;
-      totalExp += expRequired;
-    }
-    return totalExp;
-  }
+  lowerLocationConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    draggable: false,
+    vertical: true,
+    cssEase: 'cubic-bezier(0.84,0.02,0.21,1)',
+    speed: 1000,
+  };
 }
